06f67de19c71938ac67e2cb1fa286c776f730fa9
EC2 Default User <ec2-user@ip-172-31-5-225.ec2.internal>
2020-12-11 03:42:28 +0000

    Addrefile

---
+++ app/controllers/items_controller.rb
@@ -0,0 +1,20 @@
+class ItemsController < ApplicationController
+  
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    item = Item.new(item_params)
+    item.save
+    redirect_to root_path
+  end
+  
+  def index
+  end  
+  
+end
+
+def item_params
+  params.require(:item).permit(:image,:name)
+end
